9157840d35d6f90e3bb8e45f9c0378a3aef18b55
MichaCo <kletterer@web.de>
2015-04-04 23:40:33 +0200

    update

---
+++ Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="jquery-1.9.1.intellisense.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="angular.js" />
+/// <reference path="angular-messages.js" />
+/// <reference path="angular-route.js" />
+/// <reference path="angular-sanitize.js" />
+/// <reference path="angular-resource.js" />
---
+++ Scripts/index.js
@@ -0,0 +1,19 @@
+﻿
+var indexPage = angular.module('indexPage', ['ngRoute']);
+
+indexPage.controller('ContentCtrl', function ($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+});
+
+indexPage.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.when("/", {
+        templateUrl: 'index-content.html'
+    });
+    $routeProvider.when("/CacheManager", {
+        templateUrl: 'CacheManager/README.html'
+    });
+});
+
+
